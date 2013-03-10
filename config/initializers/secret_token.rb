c31c6158225868d877a7cb03286b068686dc7a07
axoloteDeAccion <mario.r.vallejo@gmail.com>
2013-03-10 16:33:22 -0600

    Deploying to server. First commit. God help us all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cel::Application.config.secret_token = '7192f083f1394f562abebc794c9697e8cdbd6129503b6b25a8a7daf4dd6fc57f31d31d32b2e8a54ca9ef7f0a0245fca84d9e14d73dd8b986e68148bc235beb33'
---
+++ test/unit/user_tokens_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserTokensTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
