a1188214ec023e36c0887d4b2e0bd36afd5d4328
axoloteDeAccion <mario.r.vallejo@gmail.com>
2013-03-11 19:28:03 -0600

    kewl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,6 @@ Cel::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.default_url_options = { :host => "christianeventlink.com"}
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
